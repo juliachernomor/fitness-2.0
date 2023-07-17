6538d9e902b9ab493e1d74450d984c998e587f41
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2023-07-17 07:50:30 +0300

    fix: faq

--- source/js/main.js
+++ source/js/main.js
@@ -3,15 +3,17 @@ import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {createNewSlider} from './vendor/swiper';
 import {initTabs} from './init-tabs';
+import {initAccordions} from './init-accordion';
 
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
 
   // Utils
-
+  initAccordions();
   iosVhFix();
   createNewSlider();
+  initTabs();
 
   // Modules
   // ---------------------------------
@@ -19,7 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initTabs();
+
     initModals();
     const form = new Form();
     window.form = form;
