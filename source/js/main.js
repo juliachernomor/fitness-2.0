8fa07ed4d105aaf6986274946b6b73a645f58592
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2023-07-17 04:47:38 +0300

    abonements

--- source/js/main.js
+++ source/js/main.js
@@ -2,22 +2,24 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {createNewSlider} from './vendor/swiper';
+import {initTabs} from './init-tabs';
 
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
 
   // Utils
-  // ---------------------------------
 
   iosVhFix();
   createNewSlider();
+
   // Modules
   // ---------------------------------
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    initTabs();
     initModals();
     const form = new Form();
     window.form = form;
